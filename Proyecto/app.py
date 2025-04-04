926c658bba4f48a6585d0aa4c0199df6e173ce5f
EsbanV <evejar2024@alu.uct.cl>
2025-04-03 22:58:50 -0300

    ya por lo menos da señales de vida

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -4,6 +4,7 @@ from rutas.categoria_rutas import categoria_rutas
 from rutas.presupuesto_rutas import presupuesto_rutas
 from rutas.transaccion_rutas import transaccion_rutas
 from rutas.usuario_rutas import usuario_rutas
+from rutas.index_rutas import index_rutas
 
 def create_app():
     app = Flask(__name__)
@@ -14,8 +15,8 @@ def create_app():
     app.register_blueprint(categoria_rutas, url_prefix='/api')
     app.register_blueprint(presupuesto_rutas, url_prefix='/api')
     app.register_blueprint(transaccion_rutas, url_prefix='/api')
-    app.register_blueprint(transaccion_rutas, url_prefix='/api')
     app.register_blueprint(usuario_rutas, url_prefix='/api')
+    app.register_blueprint(index_rutas)
 
     return app
 
